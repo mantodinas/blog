faff7cc64da3bf9c7936e86aa24cdce63b66122e
zydrunasG <29206400+zydrunasG@users.noreply.github.com>
2021-12-30 18:16:17 +0200

    Add Post model, migration, base controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        //
+    }
+
+    public function edit(Post $post)
+    {
+        //
+    }
+
+
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        //
+    }
+
+
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
