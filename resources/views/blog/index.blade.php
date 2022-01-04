cf30fab8a721e02214acb0e2f92e4b1aa85a342f
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-04 05:58:34 +0200

    Frontend update with html, scss

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -12,7 +12,7 @@
 <header>
     <div class="nav-bar-container">
         <div class="nav-bar">
-            <img src="img/logo.jpg">
+            <img src="img/fox1.png">
             <a href="#">main</a>
             <a href="#">about</a>
         </div>
@@ -20,6 +20,24 @@
 </header>
 
 <main>
+    <div class="container">
+        <h1>write blog</h1>
+        <div class="blog">
+            <h2>title</h2>
+            <p class="date">2021 12 30</p>
+            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
+            <div class="button-container">
+                <button class="btn-edit">
+                    <a href="#">edit</a>
+                </button>
+
+                <button class="btn-delete">
+                    <a href="#">delete</a>
+                </button>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <h1>write blog</h1>
         <div class="blog">
@@ -61,8 +79,8 @@
 
             <div class="footer-bottom">
                 <p>
-                    info
-                    <br> Copyright © 2021 all rights reserved
+                    Info
+                    <br> Website created for educational purposes learning and improving front end.
                     <br> Designed by Mantas Petrauskas
                 </p>
             </div>
