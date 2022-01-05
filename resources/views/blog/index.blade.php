c4788b0d97ae8ffeff72d8b2191c0368faa7b355
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-05 23:54:09 +0200

    frontend grid added on main section for multiple blogs

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -26,96 +26,56 @@
 </header>
 
 <main>
-    <div class="container">
-        <h1>blog 1</h1>
+    <div class="main-container">
         <div class="blog">
-            <h2>title</h2>
-            <p class="date">2021 12 30</p>
-            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
-            <div class="button-container">
-                <button class="btn-edit">
-                    <a href="#">edit</a>
-                </button>
+            <h2>blog 1</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
 
-                <button class="btn-delete">
-                    <a href="#">delete</a>
-                </button>
-            </div>
+        <div class="blog">
+            <h2>blog 2</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
+
+        <div class="blog">
+            <h2>blog 3</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
+
+        <div class="blog">
+            <h2>blog 4</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
+
+        <div class="blog">
+            <h2>blog 5</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
+
+        <div class="blog">
+            <h2>blog 6</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
+        </div>
+
+        <div class="blog">
+            <h2>blog 7</h2>
+            <h2>2022 01 04</h2>
+            <img src="img/logo.jpg">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sed, eaque labore impedit sint magni.</p>
         </div>
     </div>
-
-    <div class="container">
-        <h1>blog 2</h1>
-        <div class="blog">
-            <h2>title</h2>
-            <p class="date">2021 12 30</p>
-            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
-            <div class="button-container">
-                <button class="btn-edit">
-                    <a href="#">edit</a>
-                </button>
-
-                <button class="btn-delete">
-                    <a href="#">delete</a>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <h1>blog 3</h1>
-        <div class="blog">
-            <h2>title</h2>
-            <p class="date">2021 12 30</p>
-            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
-            <div class="button-container">
-                <button class="btn-edit">
-                    <a href="#">edit</a>
-                </button>
-
-                <button class="btn-delete">
-                    <a href="#">delete</a>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <h1>blog 4</h1>
-        <div class="blog">
-            <h2>title</h2>
-            <p class="date">2021 12 30</p>
-            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
-            <div class="button-container">
-                <button class="btn-edit">
-                    <a href="#">edit</a>
-                </button>
-
-                <button class="btn-delete">
-                    <a href="#">delete</a>
-                </button>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <h1>blog 5</h1>
-        <div class="blog">
-            <h2>title</h2>
-            <p class="date">2021 12 30</p>
-            <p class="description">description Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur laudantium distinctio aperiam ducimus quod maxime, earum modi esse impedit eos. Ipsa harum corrupti in odio architecto ut optio atque voluptatibus iusto quis. Ipsa obcaecati tempora ad dolores placeat, odio fuga quidem! Rem unde reiciendis, culpa debitis voluptatum magni perferendis obcaecati natus repudiandae nam, accusamus nisi earum nesciunt cumque! Molestiae quo recusandae, aperiam et ab nam repellat! Sit, vel deserunt aspernatur, soluta provident omnis delectus voluptatibus minima in blanditiis, excepturi aut laudantium enim doloribus asperiores totam dolorem voluptas. Quas, suscipit. Assumenda modi suscipit dolore cumque! Ducimus autem minima veritatis vero maiores?</p>
-            <div class="button-container">
-                <button class="btn-edit">
-                    <a href="#">edit</a>
-                </button>
-
-                <button class="btn-delete">
-                    <a href="#">delete</a>
-                </button>
-            </div>
-        </div>
-    </div>
-
 </main>
 
 <footer>
