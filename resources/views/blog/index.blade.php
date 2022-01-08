14000c5173fb9f96f24bd4ae06815b88a7266167
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-08 18:29:17 +0200

    moved index components to base template

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,82 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta charset="UTF-8">
-<title>Page Title</title>
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<style>
-</style>
-<script src="{{ asset('js/app.js') }}"></script>
-<body>
+@extends('base')
 
-<header>
-    <div class="nav-bar">
-        <a class="main-btn" href="#">main</a>
-        <a class="about-btn" href="#">about</a>
-        <input class="search-input" type="text" placeholder="Search blog...">
-        <a class="search-btn" href="#">search</a>
-        <a class="dropdown-btn" href="#"></a>
-    </div>
-
-    <div class="logo-container">
-        <div class="logo">
-            <h1 class="text1">techno</h1>
-            <h1 class="text2">blog</h1>
-        </div>
-    </div>
-</header>
-
-<main>
+@section('content')
     <div class="blog-container">
-        @foreach ($posts as $post)
-        <div class="blog">
-            <img class="blog-img" src="img/blog-example-img/blog1.jpg">
-            <h2 class="blog-title">{{ $post->title }}</h2>
-            <p class="short-description">{{ $post->description }}</p>
-            <p class="day-added">{{ $post->created_at->format('Y m d') }}</p>
+            @foreach ($posts as $post)
+            <div class="blog">
+                <img class="blog-img" src="img/blog-example-img/blog1.jpg">
+                <h2 class="blog-title">{{ $post->title }}</h2>
+                <p class="short-description">{{ $post->description }}</p>
+                <p class="day-added">{{ $post->created_at->format('Y m d') }}</p>
+            </div>
+            @endforeach
         </div>
-         @endforeach
-
-    </div>
-</main>
-
-<footer>
-    <div class="footer-container" id="about">
-        <div class="footer-top">
-            <div class="social-networks">
-                <a href="https://www.instagram.com/m.petrauskas/">
-                    <i class="fab fa-instagram fa-lg"></i>
-                </a>
-            </div>
-
-            <div class="social-networks">
-                <a href="https://twitter.com/Mantodinas">
-                    <i class="fab fa-twitter"></i>
-                </a>
-            </div>
-
-            <div class="social-networks">
-                <a href="https://www.linkedin.com/in/mantas-petrauskas-56781b142/">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-            </div>
-
-            <div class="social-networks">
-                <a href="https://github.com/mantodinas">
-                    <i class="fab fa-github"></i>
-                </a>
-            </div>
-        </div>
-
-        <div class="footer-bottom">
-            <p>
-                Info
-                <br> Website created for educational purposes learning and improving front end.
-                <br> Designed by Mantas Petrauskas
-            </p>
-        </div>
-    </div>
-</footer>
-
-</body>
-</html>
+@endsection
