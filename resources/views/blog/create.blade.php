28dca9b414987eac8623bdd23dbb371d648d78d9
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-10 00:21:13 +0200

    create blog page form frontend

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,31 +1,20 @@
 @extends('base')
 
 @section('content')
-<div class="create-blog">
-    <form class="create-container" action="" method="POST" enctype="multipart/form-data">
-        <div class="create">
-            <div class="add-img">
-                img <input type="file" name="image" accept="image/*">
-            </div>
-    
-            <div class="link">
-               link <input type="text" name="link" placeholder="link...">
-            </div>
-
-            <div class="add-title">
-
-                title <input type="text" name="title" placeholder="title...">
-            </div>
-    
-            <div class="add-content">
-                text <textarea name="content" cols="30" rows="5"></textarea>
-            </div>
-
-            <div class="add-description">
-                description <textarea name="description" cols="30" rows="5"></textarea>
-            </div>
+<div class="create-blog-container">
+    <form class="create-blog" action="" method="POST" enctype="multipart/form-data">
+            <h2>create blog</h2>
+            <label>img</label>
+            <input type="file" name="image" accept="image/*">
+            <label>link</label>
+            <input type="text" name="link" placeholder="link...">
+            <label>title</label>
+            <input type="text" name="title" placeholder="title...">
+            <label>text content</label>
+            <textarea name="content" cols="30" rows="5"></textarea>
+            <label>short description</label>
+            <textarea name="description" cols="30" rows="5"></textarea>
             <button>submit</button>
-        </div>
     </form>
 </div>
 @endsection
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,16 +1,16 @@
 @extends('base')
 
 @section('content')
-<div class="blog-to-container">
-    <div class="blog-to">
-        <form action="{{ route('post.destroy', $post->link) }}" method="POST">
-            <input type="hidden" name="_method" value="delete" />
-            <input type="submit" value="DELETE!!!!!!!">
-        </form>
-        <img src="">
+<div class="blog-page-container">
+    <form class="blog-page" action="{{ route('post.destroy', $post->link) }}" method="POST">
+        <img class="image" src="">
         <h1 class="title">{{ $post->title }}</h1>
-        <p class="description">{{ $post->description }}</p>
-        <p class="day-added">{{ $post->created_at->format('Y m d') }}</p>
-    </div>
+        <p class="content">{{ $post->content }}</p>
+        <div class="blog-bottom">
+            <input type="hidden" name="_method" value="delete" />
+            <input class="btn" type="submit" value="DELETE!!!!!!!">
+            <p class="day-created">{{ $post->created_at->format('Y m d') }}</p>
+        </div>
+    </form>
 </div>
 @endsection
