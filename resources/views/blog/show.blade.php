491b5e7be62657496076fc2615d01482d7d0e2d1
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-10 14:48:47 +0200

    show img in inner page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="blog-page-container">
     <form class="blog-page" action="{{ route('post.destroy', $post->link) }}" method="POST">
-        <img class="image" src="">
+        <img class="image" src="{{ url($post->imageLink) }}">
         <h1 class="title">{{ $post->title }}</h1>
         <p class="content">{{ $post->content }}</p>
         <div class="blog-bottom">
