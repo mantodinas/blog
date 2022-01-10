0d8b6cc6cbb50642f6652f84ffcdc60274efe840
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-10 17:30:15 +0200

    Hide post delete button when is not authenticated

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -2,21 +2,25 @@
 
 @section('content')
 <div class="blog-page-container">
-    <form class="blog-page" action="{{ route('post.destroy', $post->link) }}" method="POST">
+    <div class="blog-page">
         <img class="image" src="{{ url($post->imageLink) }}">
         <h1 class="title">{{ $post->title }}</h1>
         <p class="content">{{ $post->content }}</p>
-        <div class="blog-bottom">
-            <div class="btn-container">
-                <input type="hidden" name="_method" value="delete" />
-                <input class="btn" type="submit" value="delete">
 
-                <button>edit</button>
-            </div>
+        <div class="blog-bottom">
+            @auth
+            <form action="{{ route('post.destroy', $post->link) }}" method="POST">
+                    <div class="btn-container">
+                        <input type="hidden" name="_method" value="delete" />
+                        <input class="btn" type="submit" value="delete">
+                    </div>
+            </form>
+            @endauth
             <div class="day-created-container">
                 <p class="day-created">{{ $post->created_at->format('Y m d') }}</p>
             </div>
         </div>
-    </form>
+    </div>
 </div>
+
 @endsection
