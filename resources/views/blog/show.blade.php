238569abe2d752e68c331a1598641f5f24b3b11c
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-10 16:32:37 +0200

    small front design fixess

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -7,9 +7,15 @@
         <h1 class="title">{{ $post->title }}</h1>
         <p class="content">{{ $post->content }}</p>
         <div class="blog-bottom">
-            <input type="hidden" name="_method" value="delete" />
-            <input class="btn" type="submit" value="DELETE!!!!!!!">
-            <p class="day-created">{{ $post->created_at->format('Y m d') }}</p>
+            <div class="btn-container">
+                <input type="hidden" name="_method" value="delete" />
+                <input class="btn" type="submit" value="delete">
+
+                <button>edit</button>
+            </div>
+            <div class="day-created-container">
+                <p class="day-created">{{ $post->created_at->format('Y m d') }}</p>
+            </div>
         </div>
     </form>
 </div>
