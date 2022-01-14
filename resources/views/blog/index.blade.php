80734f18ac373981126dff6222d92e36cfdc8040
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-14 09:08:16 +0200

    blog index link wraped around  every blog, responsive grid design fix

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
     <div class="blog-container">
             @foreach ($posts as $post)
-                <div class="blog">
-                    <img class="blog-img" src="{{ $post->imageLink }}">
-                    <h2 class="blog-title">{{ $post->title }}</h2>
-                    <p class="short-description">{{ $post->description }}</p>
-                    <p class="day-added">{{ $post->created_at->format('Y m d') }}</p>
-                    <a href="{{ route('post.show', $post->link) }}">Read more</a>
-                </div>
+                <a class="link-wrap" href="{{ route('post.show', $post->link) }}">
+                    <div class="blog">
+                        <img class="blog-img" src="{{ $post->imageLink }}">
+                        <h2 class="blog-title">{{ $post->title }}</h2>
+                        <p class="short-description">{{ $post->description }}</p>
+                        <p class="day-added">{{ $post->created_at->format('Y m d') }}</p>
+                    </div>
+                </a>
             @endforeach
         </div>
 @endsection
