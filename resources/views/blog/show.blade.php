9d97f5537a51bae2b8733972e07c47f74a8205c3
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-20 19:27:24 +0200

    edit button fixed when logined

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -9,20 +9,19 @@
 
         <div class="blog-bottom">
             @auth
-            <div class="bottom-top">
-
-                <div class="btn-container">
+            <div class="buttons">
+                <div class="edit-btn-container">
                     <a href="{{ route('post.edit', $post->link) }}" class="btn" type="submit">edit</a>
                 </div>
-
-            <form action="{{ route('post.destroy', $post->link) }}" method="POST">
-                <div class="btn-container">
-                    <input type="hidden" name="_method" value="delete"/>
-                    <input class="btn" type="submit" value="delete">
-                </div>
-            </form>
-            @endauth
+    
+                <form action="{{ route('post.destroy', $post->link) }}" method="POST">
+                    <div class="delete-btn-container">
+                        <input type="hidden" name="_method" value="delete"/>
+                        <input class="btn" type="submit" value="delete">
+                    </div>
+                </form>
             </div>
+            @endauth
             <div class="day-created-container">
                 <div class="day-created">{{ $post->created_at->format('Y m d') }}</div>
             </div>
