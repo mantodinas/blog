9a335402a780779eaa9641e29694f8672d539c8f
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-24 22:25:11 +0200

    about page section created and made responsive

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <div class="about-container">
-        <h1>hello</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt voluptatum est rem officiis possimus veniam non sunt quisquam! Dolor, tenetur?</p>
+        <div class="about">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt voluptatum est rem officiis possimus veniam non sunt quisquam! Dolor, tenetur?</p>
+        </div>
     </div>
 @endsection
