d1cac158a4e77d87a6bbc24a8adb6d1e92c981a8
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-01-31 23:31:02 +0200

    create page text editor in progress

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -11,8 +11,17 @@
         <label>title</label>
         <input type="text" name="title" placeholder="title...">
         <label>short description</label>
-        <textarea name="description" cols="30" rows="5"></textarea>
+        <textarea name="description" cols="30" rows="5">
+        </textarea>
         <label>text content</label>
+            <div class="text-editor">
+                <button class="bold-text"><i class="fas fa-bold"></i></button>
+                <button class="text-underline"><i class="fas fa-underline"></i></button>
+                <button class="text-left"><i class="fas fa-align-left"></i></button>
+                <button class="text-center"><i class="fas fa-align-center"></i></button>
+                <button class="text-right"><i class="fas fa-align-right"></i></button>
+                <button class="text-color"><i class="fas fa-tint"></i></button>
+            </div>
         <textarea name="content" cols="30" rows="5"></textarea>
         <button>submit</button>
     </form>
