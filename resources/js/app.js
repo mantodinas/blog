ab03d1112f203abd298187dd09ed799bede1418f
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-02-02 00:31:11 +0200

    paragraph in progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 import 'bootstrap';
 import searchPost from './search-post';
 import initDropdown from './navbar';
+import paragraphEdit from './paragraph-edit';
 
 window.onload = () => {
     searchPost();
