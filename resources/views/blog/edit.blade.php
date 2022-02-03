7cb7749e8934b2d1bdec9954ddd8a0f21a9f807f
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-02-04 01:59:07 +0200

    fixed max lenght on link responcive look and eaqual main page blog boxes

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -8,9 +8,9 @@
         <label>image</label>
         <input type="file" name="image" accept="image/*">
         <label>link</label>
-        <input type="text" name="link" placeholder="link..." value="{{ $post->link }}">
+        <input type="text" name="link" placeholder="link..." maxlength="75" value="{{ $post->link }}">
         <label>title</label>
-        <input type="text" name="title" placeholder="title..." maxlength="10" value="{{ $post->title }}">
+        <input type="text" name="title" placeholder="title..." maxlength="75" value="{{ $post->title }}">
         <label>short description</label>
         <textarea name="description" cols="30" rows="5" maxlength="200">{{ $post->description }}</textarea>
         <label>text content</label>
