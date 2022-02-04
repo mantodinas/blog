430fbec1f853add277bcf2fd2479d90fb15ed06f
mantodinas <82802179+mantodinas@users.noreply.github.com>
2022-02-05 00:31:02 +0200

    update about page

--- resources/views/blog/about.blade.php
+++ resources/views/blog/about.blade.php
@@ -4,6 +4,9 @@
     <div class="about-container">
         <div class="about">
             <h1>about</h1>
+            <div class="img-container">
+                <img src="img/technology.png">
+            </div>
             <p class="about-paragrafh-1">“It has become appallingly obvious that our technology has exceeded our humanity.”<br> Albert Einstein
             </p>
             <p class="about-paragrafh-2">Technology has revolutionized our world and daily lives, enhanced the quality of life. Modern technology has paved the way for multi-functional devices like the smartwatch and the smartphones. Computers are increasingly faster, more portable, and higher-powered than ever before. With all of these revolutions, technology has also made our lives easier, faster, better, and more fun. 
